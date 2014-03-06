5f27ee4514cf35da4299917f2613700ef99a78e6
ibtissam <find_me@live.fr>
2014-03-06 14:17:04 +0100

    added one more line

--- ConsoleApplication9/Program.cs
+++ ConsoleApplication9/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication9
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
-
+            Console.WriteLine("Hello world again");
         }
     }
 }
